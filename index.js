b3c0b8e479d30f7b51ed4dc7f415e2daa943ae70
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-06-20 08:44:27 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+// function writeCards(name, event) {
+ function writeCards(name, event) {
+const array = []
+    for (let m = 0; m < name.length; m++) {
+        array.push(`Thank you, ${name[m]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
